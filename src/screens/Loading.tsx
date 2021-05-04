93c58d23dfb98a3432f3c7537ab6f598f6773296
kimtaegwan <ab154@naver.com>
2021-05-05 00:51:40 +0900

    Add loading page

--- src/screens/Loading.tsx
+++ src/screens/Loading.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from '@emotion/native';
 import logo from '../assets/images/logo.png';
+import LinearGradient from 'react-native-linear-gradient';
+import {StyleSheet} from 'react-native';
 
 const Container = styled.View`
-  background-color: ${({theme}: any) => theme.loadingBackground};
   flex: 1;
   align-items: center;
   justify-content: center;
@@ -12,11 +13,23 @@ const LogoImage = styled.Image`
   width: 159px;
   height: 159px;
 `;
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+  },
+});
 const Loading = () => {
   return (
-    <Container>
-      <LogoImage source={logo} />
-    </Container>
+    <LinearGradient
+      colors={['#1c1a18', '#3d3c37']}
+      style={styles.linearGradient}>
+      <Container>
+        <LogoImage source={logo} />
+      </Container>
+    </LinearGradient>
   );
 };
 
